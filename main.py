25acd58329bbcb9a1e2e328bd23f9f01ff1e7df0
Unknown <louise0927@gmail.com>
2018-09-02 19:43:58 +0800

    practice python

---
+++ main.py
@@ -0,0 +1,4 @@
+a = input("Your name:")
+a = list(reversed(a))
+b = ''.join(a)
+print ("My name is:", b)
---
+++ main2.py
@@ -0,0 +1,5 @@
+import random
+move = ["a" , "b" , "c"]
+computer = move random(0 ,2)
+player = false
+
